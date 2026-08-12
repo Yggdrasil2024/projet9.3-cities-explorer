ff6e258586cceae15ce82e29cd97442bf973a106
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-08-12 12:51:55 -0400

    feat: ajout du pont par defaut dans js"

--- pays.js
+++ pays.js
@@ -1,2 +1,6 @@
-
+// Le pont JS -> page. À recopier tel quel dans chaque projet S7 à S9.
+const sortie = document.querySelector("#sortie");
+function afficher(html) {
+  sortie.innerHTML += html;
+}
 
